dae49d2b40f3c3d15504d8418d42f497f7edcf79
Rogerson Nazário <rrnazario@gmail.com>
2025-05-25 13:19:06 +0200

    feature: get all

---
+++ Inventory/Shop.Inventory.API/Features/Products/GetAllProducts.cs
@@ -0,0 +1,47 @@
+﻿using Carter;
+using Carter.OpenApi;
+using Marten;
+using MediatR;
+using Shop.Inventory.Domain.Products;
+
+namespace Shop.Inventory.API.Features.Products;
+
+public static class GetAllProducts
+{
+    internal record Query : IRequest<IReadOnlyCollection<Product>>;
+
+    private record Handler : IRequestHandler<Query, IReadOnlyCollection<Product>>
+    {
+        private readonly IQuerySession _session;
+
+        public Handler(IQuerySession session)
+        {
+            _session = session;
+        }
+
+        public async Task<IReadOnlyCollection<Product>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var products = await _session.Query<Product>()
+                .ToListAsync(cancellationToken);
+
+            return products;
+        }
+    }
+}
+
+public class GetAllProductsModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet(
+            "/products",
+            async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetAllProducts.Query());
+
+                return Results.Ok(result);
+            })
+            .IncludeInOpenApi()
+            .Produces<Product[]>();
+    }
+}
